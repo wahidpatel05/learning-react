6fe77a12600ae81feacff9a61b79d099a1853b7f
Wahid Patel <patel.wahid2005@gmail.com>
2025-08-02 18:28:40 +0530

    hooks and counter

--- 01basics/src/main.jsx
+++ 01basics/src/main.jsx
@@ -1,10 +1,19 @@
-
+import React from 'react'
 import { createRoot } from 'react-dom/client'
 
 import App from './App.jsx'
 
+const areactElement = React.createElement(
+    'a',
+    {href : "https://todo-list-murex-tau-19.vercel.app/" , target : "_blank"},
+    "Check out my project made using .js"
+)
+
 createRoot(document.getElementById('root')).render(
- 
+    <>
+    {areactElement}
     <App />
+    </>
+    
  
 )
