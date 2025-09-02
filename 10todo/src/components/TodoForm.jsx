a0e7ea797bbd3e8b764a15a8eec3055150037af1
Wahid Patel <patel.wahid2005@gmail.com>
2025-09-02 12:00:09 +0530

    tod context api

---
+++ 10todo/src/components/TodoForm.jsx
@@ -0,0 +1,29 @@
+import React, { useState, useTransition } from 'react'
+import { UseTodo } from '../context/TodoContexts'
+
+function TodoForm() {
+    const [todo, settodo] = useState("")
+    const {addTodo} = UseTodo()
+
+    const add = (e) => {
+        e.preventDefault()
+        if (!todo) return 
+        addTodo({todo, completed: false})
+        settodo("")
+    }
+    
+  return (
+    <form onSubmit={add} className='flex'>
+        <input type="text" 
+        className='w-full flex items-center justify-center border border-black/10 rounded-l-lg px-3 outline-none duration-150 bg-white/20 py-1.5'
+        placeholder='Write Todo...'
+        value={todo}
+        onChange={(e) => settodo(e.target.value)}
+        />
+        <button type='submit'
+        className='rounded-r-lg px-3 py-1 bg-green-600 text-white shrink-0'>Add</button>
+    </form>
+  )
+}
+
+export default TodoForm
---
+++ 10todo/src/context/index.js
@@ -0,0 +1,1 @@
+import { TodoContext, TodoProvider, UseTodo } from "./TodoContexts";
